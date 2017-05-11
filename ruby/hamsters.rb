f825c5c6231fcc37ebaf7c397aee5988348c9ff4
Anthony Haler <anthonyhaler@Anthonys-MacBook-Air.local>
2017-05-11 18:32:10 -0400

    Add user hamster stats add if statment add integer/string change

---
+++ ruby/hamsters.rb
@@ -0,0 +1,18 @@
+puts "What is the Hamsters name?"
+hamster_name = gets.chomp
+
+puts "On a scale from 1-10 how loud is the hamster (one being the lowest)? "
+hamster_volume = gets.chomp.to_i
+
+puts "What color fur does the hamster have? "
+fur_color = gets.chomp
+
+puts "Is the hamster good for adoption (yes/no)? "
+adoption_fit = gets.chomp
+
+puts "How old is the hamster?"
+age = gets.chomp.to_i 
+
+if age == 0 
+  age = nil
+end 
