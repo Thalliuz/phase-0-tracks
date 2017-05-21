ca13044de533e402853e9c9487bc52157cce4dbf
Anthony Haler <anthonyhaler@Anthonys-MacBook-Air.local>
2017-05-21 00:10:51 -0400

    Add final changes to the challenge

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,34 @@
+jail = {
+
+"Gaurd Room" => [
+  'Table',
+  'chairs',
+  'guns',
+  'tv'],
+
+  "Cell Block" => [
+    'cell1',
+    'cell2',
+    'cell3',
+    'cell4'],
+
+  "Lunch Room" => [
+    'table1',
+    'table2',
+    'table3',
+    'table4',
+    'chairs',
+    'trays'
+  ],
+
+"Max Security" => [
+  'cell1',
+  'cell2',
+  'cell3',
+  'cell4'
+  ]
+
+}
+
+puts jail["Gaurd Room"][0]
+puts jail["Lunch Room"][2]
