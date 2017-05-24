30e4bf7a1e122565edb93d4e59481fb8ca6efbe6
Anthony Haler <anthonyhaler@Anthonys-MacBook-Air.local>
2017-05-24 18:59:24 -0400

    Add own class and instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,8 @@
 class Puppy
+  
+  def initialize
+    print "initializing new puppy instance..."
+  end
 
   def fetch( toy )
     puts "I brought back the #{toy}!"
@@ -21,14 +25,52 @@ class Puppy
     print "Fido sat down."
   end
 end
-fido = Puppy.new
-puts 
-fido.fetch("ball")
-puts
-fido.speak(5)
-puts 
-fido.age(6)
-puts
-fido.roll_over
-puts
-fido.sit
+#Driver Code
+#fido = Puppy.new
+#puts 
+#fido.fetch("ball")
+#puts
+#fido.speak(5)
+#puts 
+#fido.age(6)
+#puts
+#fido.roll_over
+#puts
+#fido.sit
+
+
+class Batman 
+  def initialize
+    puts "Cooking in the batcave"
+  end 
+  
+  def call_batmobile( location )
+  puts "Alfred send batmobile to #{location}"
+  end 
+
+  def suit_up
+    puts "Getting ready for the big show."
+  end 
+
+end
+
+batman_clones = []
+
+50.times do 
+  
+  batman_clones << Batman.new
+
+end 
+
+batman_clones.each do | clone |
+  
+  clone.call_batmobile( "Italy" )
+  
+  clone.suit_up
+ 
+  end 
+
+
+
+
+
