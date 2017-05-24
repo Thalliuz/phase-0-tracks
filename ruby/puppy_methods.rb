52e7855d88369ebb227564f11318eefb571ee355
Anthony Haler <anthonyhaler@Anthonys-MacBook-Air.local>
2017-05-24 18:28:59 -0400

    Add all of the methods for class Puppy

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,34 @@
 class Puppy
 
-  def fetch(toy)
+  def fetch( toy )
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak( woof_times )
+    woof_times.times { print "Woof!"}
+  end 
 
+  def roll_over
+    print "roll over"
+  end 
+  
+  def age( human_years )
+    print human_years * 7
+  end 
+  
+  def sit
+    print "Fido sat down."
+  end
 end
-
+fido = Puppy.new
+puts 
+fido.fetch("ball")
+puts
+fido.speak(5)
+puts 
+fido.age(6)
+puts
+fido.roll_over
+puts
+fido.sit
