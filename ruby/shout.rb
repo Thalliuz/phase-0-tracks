33d41c65b97853a33b91948d1f9b1814210db8c2
Anthony Haler <anthonyhaler@Anthonys-MacBook-Air.local>
2017-05-29 01:12:23 -0400

    Add flight.rb classes and modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,15 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words + "<3<3<3" + " :)"
+    
+  end
 end
+
+# ----driver code-----
+puts Shout.yell_angrily("Get out of my yard")
+puts Shout.yelling_happily("I love coding")
