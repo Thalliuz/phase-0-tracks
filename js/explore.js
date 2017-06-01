f9c872a6f56361216256c9b27b47dd01713f171b
Anthony Haler <anthonyhaler@Anthonys-MacBook-Air.local>
2017-06-01 15:35:18 -0400

    add split reverse and join to make the string reversed

--- js/explore.js
+++ js/explore.js
@@ -6,8 +6,9 @@
 // add a if statment that will print the function if true
 //set reverse_string equal to a new variable 
 
+
 function reverse_string(string) {
   
-  return string
-}    
-    console.log("Sloths Are The Best")
+  return string.split("").reverse().join("")
+  }
+    console.log(reverse_string("Sloths Are The Best"))
