5182ddc9000d0458b9fef0c2e1c8effa6d5918dd
Anthony Haler <anthonyhaler@Anthonys-MacBook-Air.local>
2017-06-05 23:30:17 -0400

    finished 7.3 week 7

--- js/algos.js
+++ js/algos.js
@@ -1,39 +1,60 @@
-var array = ["hello", "goodbye", "long", "time", "no", "see"]
-array.sort();
-console.log(array)
-function returnGoodbye(array) {
-var longestSring = array[0];
-return longestSring;
-}
+//var array = ["hello", "goodbye", "long", "time", "no", "see"]
+//console.log(array)
+var array = []
+//create a function checking to see which of the words in the array are the longest. 
+//return the longest
+function returnBiggestWord(array) {
+  var longestIndex = 0 
+  for (index = 0; index < array.length; index++) {
+    if (array[index].length > array[longestIndex].length)
+      longestIndex = index}
+
+return array[longestIndex]}
+
 // Driver code
-console.log(returnGoodbye(array));
+console.log(returnBiggestWord(array));
 
-// need to figure this out.
-//var blah = ({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3});
-//console.log(andAgain)
+// function checks to see if the compared objects are true or false 
+//share at least one key-value pair
+function objectCheck(object1, object2) {
+  for (key1 in object1) {
+    for (key2 in object2){
+      if (key1 == key2 && object1[key1] == object2[key2]){
+        return true 
+      }
+    }
+
+  }
+return false 
+}
+
+//driver code
+console.log(objectCheck({name: "Steven", age: 54}, {name: "Tamir", age: 54})); //true
+console.log(objectCheck({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3})); 
+//true
 
 
-//builds and returns an array of strings of the given length. So if we ran your function with an argument of 3, we would get an array of 3 random words back (the words don't have to be actual sensical English words -- "nnnnfph" totally counts). The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters. (This involves a new trick, generating a random number, that you'll have to look up, but the solution on how to do so is relatively straightforward.)
-//Write a function that takes an integer for length
+// create a function tht takes in an integer and returns an array with random words 
 function randomWordsArray(intForLength) {
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var array = [];
-for (i = 0; i < intForLength; i++) {
-  var randomWord = ""
-  var randomNumber = Math.floor(Math.random() * (11 - 1)) + 1;
-for ( letter = 0; letter < randomNumber; letter++){
-var randomLetterIndex = Math.floor(Math.random() * (alphabet.length - 0))
-  randomWord += alphabet[randomLetterIndex]
-  }
-  array.push(randomWord)
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var array = [];
+  for (i = 0; i < intForLength; i++) {
+    var randomWord = ""
+    var randomNumber = Math.floor(Math.random() * (11 - 1)) + 1;
+  for ( letter = 0; letter < randomNumber; letter++){
+    var randomLetterIndex = Math.floor(Math.random() * (alphabet.length - 0))
+    randomWord += alphabet[randomLetterIndex]
+    }
+    array.push(randomWord) }
   
-  console.log(array)
-  }
-  return array
-  }
-  console.log(randomWordsArray(4))
+  return array}
+  //TEST//console.log(randomWordsArray(4))
   
 
-//how to make a random letter in javascript 
 
-//loop 3 times
+// loop to run through the two funcions 
+for (i = 1; i < 11 ; i++){
+   var newWordsArray=randomWordsArray(i)
+   console.log(newWordsArray)
+   console.log(returnBiggestWord(newWordsArray))
+}
